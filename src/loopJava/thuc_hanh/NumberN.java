1211075b05ab069615794fb996decd7a8b8ad098
unknown <NgocAnh0904>
2020-10-21 09:10:22 +0700

    src

---
+++ src/loopJava/thuc_hanh/NumberN.java
@@ -0,0 +1,12 @@
+package loopJava.thuc_hanh;
+
+import java.util.Scanner;
+
+public class NumberN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+    }
+}
